10554dc98d79de2cef17365ca368cfdf05c84ff6
Srikanth <srikanthdevopsworld@gmail.com>
2025-07-27 22:55:17 +0530

    array script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,8 +2,8 @@
 
 movies=("RRR" "Pushpa" "SALAAR")
 
-echo "first movie is : $movies[0]"
-echo "second movie is : $movies[1]"
-echo "Third movie is : $movies[2]"
+echo "first movie is : ${movies[0]}"
+echo "second movie is : ${movies[1]}"
+echo "Third movie is : ${movies[2]}"
 
 echo "all movies are: ${movies[@]}"

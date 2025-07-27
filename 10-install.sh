6bf503b56d4eec47606bf089f2bf6aac3972aecb
Srikanth <srikanthdevopsworld@gmail.com>
2025-07-28 00:11:25 +0530

    install sql

---
+++ 10-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+userid=$(id -u)
+
+if [ $userid -ne 0]
+   then 
+   echo " you must have sudo privileges"
+fi
+
+dnf list installed mysql
+
+if [ $? ne 0]
+  then
+      dnf install mysql -y
+        if [ $? ne 0]
+        echo "Installing Mysql FAILED"
+        exit 1
+            else
+        echo "Mysql installed successfully..."
+        fi    
+   

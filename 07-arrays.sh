4e907fe13a9333c668265e9eb40f39d4d664b493
Srikanth <srikanthdevopsworld@gmail.com>
2025-07-27 22:53:47 +0530

    array script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -6,4 +6,4 @@ echo "first movie is : $movies[0]"
 echo "second movie is : $movies[1]"
 echo "Third movie is : $movies[2]"
 
-echo "all movies are: ${movies(@)}"
+echo "all movies are: ${movies[@]}"

b5e1453cd7f567818b52b1411c4671cb0638ac7f
Srikanth <srikanthdevopsworld@gmail.com>
2025-07-28 16:24:27 +0530

    functions

---
+++ 12-colors.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+VALIDATE() {
+
+            if [ $1 -ne 0 ]
+            then    
+                echo "$2 .... $R FAILURE"
+                exit 1
+            else
+                echo "$2 .... $G SUCCESS"    
+            fi
+            }
+
+if [ $USERID -ne 0 ]
+ then
+ echo "User must have sudo privileges"
+    exit 1
+fi
+
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? "Installin mysql"
+else
+    echo "mysql is already $Y installed"
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "Installin git"
+else
+    echo "git is already $Y installed"
+fi

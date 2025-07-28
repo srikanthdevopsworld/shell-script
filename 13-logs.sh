44388ddcf85d0ca407fca06fd2429aa98e3097ae
Srikanth <srikanthdevopsworld@gmail.com>
2025-07-28 17:03:02 +0530

    functions

--- 13-logs.sh
+++ 13-logs.sh
@@ -4,7 +4,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 LOG_FOLDER="/var/log/shell-script-logs"
-LOG_FILE=$(echo $0 | cut "." -f1)
+LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 VALIDATE() {

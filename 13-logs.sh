85a1724cc17cad8ee2a6a5899b5be3ceda5c8a3c
Srikanth <srikanthdevopsworld@gmail.com>
2025-07-28 17:00:56 +0530

    functions

---
+++ 13-logs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOG_FOLDER="/var/log/shell-script-logs"
+LOG_FILE=$(echo $0 | cut "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+VALIDATE() {
+
+            if [ $1 -ne 0 ]
+            then    
+                echo -e "$2 .... $R FAILURE $N"
+                exit 1
+            else
+                echo -e "$2 .... $G SUCCESS $N"    
+            fi
+            }
+
+
+echo "scripted started executing at: $TIMESTAMP &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+ then
+ echo -e "User must have sudo privileges"
+    exit 1
+fi
+
+
+dnf list installed mysql &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y &>>$LOG_FILE_NAME
+    VALIDATE $? "Installin mysql"
+else
+    echo -e "mysql is already $Y installed $N"
+fi
+
+dnf list installed git &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y &>>$LOG_FILE_NAME
+    VALIDATE $? "Installin git"
+else
+    echo -e "git is already $Y installed $N"
+fi

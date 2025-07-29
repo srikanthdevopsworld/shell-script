973ab5d70d756e2cc1795a1226b6896f06fbb947
Srikanth <srikanthdevopsworld@gmail.com>
2025-07-29 13:07:01 +0530

    backup

--- 18-backup.sh
+++ 18-backup.sh
@@ -13,22 +13,32 @@ LOG_FOLDER="/home/ec2-user/app-logs"
 LOG_FILE=$(echo #0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S )
 LOG_FILE_NAME=$($LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log)
-USAGE()
-{
+USAGE(){
     echo -e " $R Usage:: $N backup-file-name.sh <source-dir> <destination-dir> <days>
     exit 1
 }
 
 if [ $# -lt 2 ]
  then 
-  echo "Usage of the code is: $USAGE"
-  exit 1
+  USAGE
 fi
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime $DAYS )
 echo "Files to delete are: $FILES_TO_DELETE"
-mkdir /home/ec2-user/app-logs
-mkdir /home/ec2-user/archive-logs
+mkdir -p /home/ec2-user/app-logs
+mkdir -p /home/ec2-user/archive-logs
+
+if [ ! -d "$SOURCE_DIR" ]
+    then
+        echo -e "$R $SOURCE_DIR does not exist.. please check $N"
+        exit 1
+fi
+if [ ! -d "$SOURCE_DIR" ]
+    then
+        echo -e "$R $DESTINATION_DIR does not exist.. please check $N"
+        exit 1
+fi
+
 
 
 

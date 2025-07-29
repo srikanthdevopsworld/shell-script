798ca57da11cbc8ba7f1ad2d907a4af8f6ff6103
Srikanth <srikanthdevopsworld@gmail.com>
2025-07-29 13:31:49 +0530

    backup

--- 18-backup.sh
+++ 18-backup.sh
@@ -41,6 +41,25 @@ fi
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS )
 
-echo "Files to delete are: $FILES_TO_DELETE"
+if [ -n $FILES_TO_DELETE]
+then
+    echo "files are $FILES_TO_DELETE"
+    ZIP_FILE="$DESINATION_DIR/app-logs-$TIMESTAMP.zip"
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
+    if [ -f "$ZIP_FILE"]
+     then
+     echo -e "successfully created zip files for older than $DAYS "
+        while read -r filepath
+        do
+        echo "deleting file: $filepath"
+        done <<< $FILES_TO_DELETE
+    else
+        echo -e "$R ERROR: $N Failed to create ZIP files"
+        exit 1
+    fi
+
+else
+    echo "No files to delete older than $DAYS"
+fi
 
 

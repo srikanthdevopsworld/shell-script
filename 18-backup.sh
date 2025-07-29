39d364337c01f6b22c6038273d9a6c0dacccf729
Srikanth <srikanthdevopsworld@gmail.com>
2025-07-29 12:55:35 +0530

    backup

---
+++ 18-backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DESTINATION_DIR=$2
+DAYS={3-14} #Optional
+
+LOG_FOLDER="/home/ec2-user/app-logs"
+LOG_FILE=$(echo #0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S )
+LOG_FILE_NAME=$($LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log)
+USAGE()
+{
+    sh backup-file-name.sh <source-dir> <destination-dir> <days>
+}
+
+if [ $# -lt 2 ]
+ then 
+  echo "Usage of the code is: $USAGE"
+  exit 1
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime $DAYS )
+echo "Files to delete are: $FILES_TO_DELETE"
+mkdir /home/ec2-user/app-logs
+mkdir /home/ec2-user/archive-logs
+
+
+

dd93a9145ed0377d1db8270d78687086ce398f7d
Srikanth <srikanthdevopsworld@gmail.com>
2025-07-29 16:34:54 +0530

    backup

--- 18-backup.sh
+++ 18-backup.sh
@@ -42,26 +42,26 @@ fi
 
 FILES_TO_DELETE=$( find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-if [ -n "$FILES_TO_DELETE" ]
-then
-        echo "files are $FILES_TO_DELETE"
-        ZIP_FILE="$DESTINATION_DIR/app-logs-$TIMESTAMP.zip"
-        find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
-    if [ -f "$ZIP_FILE" ]
-     then
-     echo -e "successfully created zip files for older than $DAYS "
-        while read -r filepath
-        do
-        echo "deleting file: $filepath" &>>$LOG_FILE_NAME
-        rm -rf $filepath
-        done <<< $FILES_TO_DELETE
-    else
-        echo -e "$R ERROR: $N Failed to create ZIP files"
-        exit 1
-    fi
+# if [ -n "$FILES_TO_DELETE" ]
+#     then
+#         echo "files are $FILES_TO_DELETE"
+#         ZIP_FILE="$DESTINATION_DIR/app-logs-$TIMESTAMP.zip"
+#         find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
+#     if [ -f "$ZIP_FILE" ]
+#      then
+#      echo -e "successfully created zip files for older than $DAYS "
+#         while read -r filepath
+#         do
+#         echo "deleting file: $filepath" &>>$LOG_FILE_NAME
+#         rm -rf $filepath
+#         done <<< $FILES_TO_DELETE
+#     else
+#         echo -e "$R ERROR: $N Failed to create ZIP files"
+#         exit 1
+#     fi
 
-else
-    echo "No files to delete older than $DAYS"
-fi
+# else
+#     echo "No files to delete older than $DAYS"
+# fi
 
 

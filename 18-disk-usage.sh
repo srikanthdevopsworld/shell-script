15fdfcbba056c111c9d9b395b70ab195735928b0
Srikanth <srikanthdevopsworld@gmail.com>
2025-07-30 12:21:09 +0530

    backup

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-USAGE=$(df -h | awk -F " " '{print $5F}" | cut -d "%" -f1 )
+USAGE =$( df -hT | grep xfs)
+
+#awk -F " " '{print $5F}" | cut -d "%" -f1)
 
 echo "disk usage is: $USAGE "
 

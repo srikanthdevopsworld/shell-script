2d6cbe2a900bcc2638de920675db103c6ea522a8
Srikanth <srikanthdevopsworld@gmail.com>
2025-07-30 12:35:35 +0530

    backup

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -2,7 +2,7 @@
 
 DISK_USAGE=$( df -hT | grep xfs)
 DISK_THRESHOLD=5
-
+MSG " "
 
 while read -r line
 do
@@ -11,6 +11,12 @@ do
 USAGE=$(echo $line | awk -F " " '{print $6F}')
 PARTITION=$(echo $line | awk -F " " '{print $NF}')
 
-echo $USAGE of the Partition is $PARTITION
+#echo $USAGE of the Partition is $PARTITION
+
+    if [ $USAGE -ge $DISK_THRESHOLD]
+        then 
+        MSG+="high disk utilization on partition $PARTITION and usage is $USAGE <br>"
+    fi
 
 done <<< $DISK_USAGE
+echo -e "Message: $MSG

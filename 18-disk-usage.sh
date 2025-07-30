9a25666fb853e46fac98e05fcd2bf3ab6a67afaa
Srikanth <srikanthdevopsworld@gmail.com>
2025-07-30 12:27:59 +0530

    backup

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-USAGE=$( df -hT | grep xfs)
+DISK_USAGE=$( df -hT | grep xfs)
+DISK_THRESHOLD=5
 
-#awk -F " " '{print $5F}" | cut -d "%" -f1)
 
 while read -r line
 do
-echo "disk usage is: $line "
 
-done <<< $USAGE
+
+USAGE=$(echo $line | awk -F " " '{print $6F}')
+
+done <<< $DISK_USAGE
